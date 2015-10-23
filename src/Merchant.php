6997451b5ab43de4842404b5657fe5f856ccac38
Andrii Vasyliev <sol@hiqdev.com>
2015-10-23 12:15:53 +0000

    inited

---
+++ src/Merchant.php
@@ -0,0 +1,61 @@
+<?php
+
+/*
+ * ePayService plugin for PHP merchant library
+ *
+ * @link      https://github.com/hiqdev/php-merchant-epayservice
+ * @package   php-merchant-epayservice
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2015, HiQDev (https://hiqdev.com/)
+ */
+
+namespace hiqdev\php\merchant\epayservice;
+
+class Merchant extends \hiqdev\php\merchant\Merchant
+{
+    protected static $_defaults = [
+        'name'        => 'epayservice',
+        'label'       => 'ePayService',
+        'actionUrl'   => 'https://online.epayservices.com/merchant/index.php',
+        'confirmText' => 'OK',
+    ];
+
+    public function getDescription()
+    {
+        return strtr(parent::getDescription(), '_@', '-:');
+    }
+
+    public function getInputs()
+    {
+        return [
+            'EPS_DESCRIPTION'   => $this->description,
+            'EPS_GUID'          => $this->purse,
+            'EPS_AMOUNT'        => $this->sum,
+            'client'            => $this->username,
+            'EPS_RESULT_URL'    => $this->confirmUrl,
+            'EPS_SUCCESS_URL'   => $this->successUrl,
+            'EPS_FAIL_URL'      => $this->failureUrl,
+        ];
+    }
+
+    public function validateConfirmation($data)
+    {
+        if ($data['EPS_RESULT']=='fail') {
+            return 'failed at paysystem';
+        }
+        $str = $data['EPS_AMOUNT'].$this->purse.$this->secret;
+        if (md5($str) != strtolower($data['check_key'])) {
+            return 'wrong hash';
+        }
+        if ($data['EPS_RESULT']!='done') {
+            die(json_encode('OK')); ### THIS IS THE RETURN FOR SYSTEM
+        }
+        $this->mset(array(
+            'from'  => $data['EPS_ACCNUM'],
+            'txn'   => $data['EPS_TRID'],
+            'sum'   => $data['EPS_AMOUNT'],
+            'time'  => date('c'),
+        ));
+        return null;
+    }
+}
